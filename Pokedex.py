03011c1c9280367c1092a50d0505573b46b1683e
Jasmine Liu <43485610+jasminehliu@users.noreply.github.com>
2018-09-21 21:10:29 -0400

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -1,24 +1,20 @@
-#################################################
-# rock_paper_scissors.py
-#################################################
-"""I wanna be the very best
-Like no one ever was
-To catch them is my real test
-To train them is my cause"""
+import math
+import string
+import copy
 
-import random
 from tkinter import *
-
-def runWindow(winWidth=500, winHeight=500):
+    
+def runWindow(winWidth = 500, winHeight = 500):
     root = Tk()
     root.resizable(width=False, height=False) # prevents resizing window
     canvas = Canvas(root, width=winWidth, height=winHeight)
     canvas.configure(bd=0, highlightthickness=0)
     canvas.pack()
-  
+    
+    root.mainloop()
+
 def main():
     runWindow()
 
 if __name__ == '__main__':
     main()
-#
