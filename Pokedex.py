b09040d174155f5d9921ebffda920bc9eb57e20c
Jasmine Liu <43485610+jasminehliu@users.noreply.github.com>
2018-09-21 21:02:18 -0400

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -4,4 +4,17 @@
 #Hi, this is Gwyneth
 #Hello fellow hAcKErS, this is eRiC LIu
 import random
+
+def runWindow(winWidth=500, winHeight=500):
+    root = Tk()
+    root.resizable(width=False, height=False) # prevents resizing window
+    canvas = Canvas(root, width=winWidth, height=winHeight)
+    canvas.configure(bd=0, highlightthickness=0)
+    canvas.pack()
+  
+def main():
+    runWindow()
+
+if __name__ == '__main__':
+    main()
 #
