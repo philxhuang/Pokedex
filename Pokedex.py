ee6fb9f9ba494329eaaff6d89a938ffe911963f9
Phil Huang <42421010+philhuang99@users.noreply.github.com>
2018-09-22 12:22:50 -0400

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -30,9 +30,26 @@ pokemonList = (
 )
 desList = (
 ('For some time after its birth, it grows by \n gaining nourishment from the seed on its back.'),
-('When the bud on its back starts swelling, a sweet aroma wafts to indicate the flowers coming bloom.'),
-('After a rainy day, the flower on its back smells stronger. The scent attracts other Pokémon.'),
-
+('When the bud on its back starts swelling, \n a sweet aroma wafts to indicate the flowers coming bloom.'),
+('After a rainy day, the flower on its back \n smells stronger. The scent attracts other Pokémon.'), #3
+('The fire on the tip of its tail is a measure \n of its life. If healthy, its tail burns intensely.'),
+('In the rocky mountains where Charmeleon live, \n their fiery tails shine at night like stars.'),
+('It is said that Charizards fire burns hotter \n if it has experienced harsh battles.'),
+('It shelters itself in its shell then strikes \n back with spouts of water at every opportunity.'),
+('It is said to live 10,000 years. Its furry \n tail is popular as a symbol of longevity.'),
+('The jets of water it spouts from the rocket \n cannons on its shell can punch through thick steel.'),
+('It releases a stench from its red antenna to \n repel enemies. It grows by molting repeatedly.'),#10
+('A steel-hard shell protects its tender body. \n It quietly endures hardships while awaiting evolution.'),
+('It loves the honey of flowers and can locate flower \n patches that have even tiny amounts of pollen.'),
+('It eats its weight in leaves every day. \n It fends off attackers with the needle on its head.'),
+('While awaiting evolution, it hides from \n predators under leaves and in nooks of branches.'),
+('Its best attack involves flying around at high speed, \n striking with poison needles, then flying off.'),#15
+('It is docile and prefers to avoid conflict. \n If disturbed, however, it can ferociously strike back.'),
+('It flies over its wide territory in search of prey, \n downing it with its highly developed claws.'),
+('It flies over its wide territory in search of prey, \n downing it with its highly developed claws.'),
+('It searches for food all day. It gnaws on hard objects \n to wear down its fangs, which grow constantly during its lifetime.'),
+('With its long fangs, this surprisingly violent Pokémon \n can gnaw away even thick concrete with ease.'),#20
+('It flaps its small wings busily to fly. \n Using its beak, it searches in grass for prey.'),
 )
 #===============================================================
 def runWindow(winWidth = 800, winHeight = 600):
@@ -65,8 +82,8 @@ def runWindow(winWidth = 800, winHeight = 600):
     canvas.create_text(70, 210, font="Verdana 12", text="Evolve\nLevel")
     canvas.create_text(70, 290, font="Verdana 12", text="Egg\nGroups")
     
-    canvas.create_text(90, 520, font="Verdana 12", text="Pokedex\nDescription")
-    des = canvas.create_text(420, 520, font="Verdana 14 italic", text='')
+    canvas.create_text(90, 530, font="Verdana 12", text="Pokedex\nDescription")
+    des = canvas.create_text(430, 530, font="Verdana 12 italic", text='')
 #================================================
     def displayinfo(event):
         current = pokelist.curselection()
@@ -80,7 +97,7 @@ def runWindow(winWidth = 800, winHeight = 600):
             
             nonlocal des
             canvas.delete(des)
-            des = canvas.create_text(420, 520, font="Verdana 14 italic", text=desList[current[0]])
+            des = canvas.create_text(430, 530, font="Verdana 12 italic", text=desList[current[0]])
 #================================================
     
     pokelist.bind("<<ListboxSelect>>", displayinfo)
