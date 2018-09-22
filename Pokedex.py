dd27e47d781114604c308048f475fba2b1299500
Gwyneth-Chen <43485598+Gwyneth-Chen@users.noreply.github.com>
2018-09-22 09:49:16 -0400

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -7,9 +7,22 @@ from tkinter import *
 def runWindow(winWidth = 800, winHeight = 800):
     root = Tk()
     root.resizable(width=False, height=False) # prevents resizing window
+    scrollbar = Scrollbar(root, bd=0)
+    pokelist = Listbox(root, yscrollcommand = scrollbar.set, selectmode=BROWSE)
+    pokelist.insert(END, "001", "Bulbasaur")
+    pokelist.insert(END, "001", "Bulbasaur")
+    pokelist.insert(END, "001", "Bulbasaur")
+    pokelist.insert(END, "001", "Bulbasaur")
+    pokelist.insert(END, "001", "Bulbasaur")
+    pokelist.insert(END, "001", "Bulbasaur")
+    pokelist.insert(END, "001", "Bulbasaur")
+    scrollbar.config(command=pokelist.yview)
+    scrollbar.pack(side=LEFT, fill=Y)
+    pokelist.pack(side=LEFT, fill=Y)
     canvas = Canvas(root, width=winWidth, height=winHeight)
     canvas.configure(bd=0, highlightthickness=0)
-    canvas.pack()
+    canvas.pack(side=RIGHT, fill=BOTH)
+     
     
     root.mainloop()
 
