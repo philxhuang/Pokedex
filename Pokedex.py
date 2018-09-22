0fd831d454ec2cbef4aa4f41d2bdd9f5cd743945
Jasmine <43485610+jasminehliu@users.noreply.github.com>
2018-09-22 09:24:30 -0400

    ignore change
    thing

--- Pokedex.py
+++ Pokedex.py
@@ -1,20 +1,20 @@
-#################################################
-# rock_paper_scissors.py
-#################################################
-#Hello fellow hAcKErS, this is eRiC LIu
-import random
-from tkinker import *
+import math
+import string
+import copy
 
-def runWindow(winWidth=500, winHeight=500):
+from tkinter import *
+    
+def runWindow(winWidth = 500, winHeight = 500):
     root = Tk()
     root.resizable(width=False, height=False) # prevents resizing window
     canvas = Canvas(root, width=winWidth, height=winHeight)
     canvas.configure(bd=0, highlightthickness=0)
     canvas.pack()
-  
+    
+    root.mainloop()
+
 def main():
     runWindow()
 
 if __name__ == '__main__':
     main()
-#
