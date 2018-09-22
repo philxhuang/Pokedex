19ef0e78ef55f8d5ed599e66fffcf5a2bacd357c
Gwyneth-Chen <43485598+Gwyneth-Chen@users.noreply.github.com>
2018-09-21 21:03:35 -0400

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -1,7 +1,6 @@
 #################################################
 # rock_paper_scissors.py
 #################################################
-#Hi, this is Gwyneth
 #Hello fellow hAcKErS, this is eRiC LIu
 import random
 
